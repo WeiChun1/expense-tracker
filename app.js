21181b8cb2dd9fa7fbba57ee320991ac5c21b1f3
andybox111 <andybox11111@gmail>
2023-09-06 16:38:47 +0800

    feat: add CRUD function

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ const exphbs = require('express-handlebars')
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
+const handlebarsHelpers = require('./helpers/handlebars-helpers')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 const routes = require('./routes')
 const app = express()
 require('./config/mongoose')
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs', helpers: handlebarsHelpers }))
 app.set('view engine', 'hbs')
 
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 app.use(routes)
 
 app.listen(process.env.PORT, () => {
--- routes/modules/record.js
+++ routes/modules/record.js
@@ -4,7 +4,26 @@ const router = express.Router()
 
 const Record = require('../../models/record')
 const Category = require('../../models/category')
+const category = require('../../models/category')
 
+
+router.get('/new', (req, res) => {
+  Category.find().lean()
+    .then(categories => res.render('new', { categories }))
+  
+})
+router.post('/', (req, res) => {
+  const {categoryId, amount, date, name} = req.body
+  Record.create({
+    categoryId,
+    userId: categoryId,
+    amount,
+    date,
+    name
+  })
+  .then(() => res.redirect('/'))
+  .catch(err => console.log(err))
+})
 router.get('/:category', (req, res) => {
   const category = req.params.category
   Category.findOne({ name_en: category })
@@ -24,4 +43,40 @@ router.get('/:category', (req, res) => {
     .catch(error => console.error(error)) 
 })
 
+router.get('/:id/edit', (req, res) => {
+  const _id = req.params.id
+  Promise.all([
+    Record.findOne({ _id }).lean(),
+    Category.find().lean()
+  ])
+    .then(([record, categories]) => {
+      Category.findOne({ _id: record.categoryId }).lean()
+      .then(category =>{
+        return record.categoryId.name = category.name
+      })
+      .then(() => res.render('edit', { record, categories }))
+    })
+    .catch(error => console.log(error))
+})
+router.put('/:id', (req, res) => {
+  const { categoryId, amount, date, name } = req.body
+  const _id = req.params.id
+  Record.findOne({ _id })
+    .update({
+      categoryId,
+      userId: categoryId,
+      amount,
+      date,
+      name
+    })
+    .then(() => res.redirect('/'))
+    .catch(err => console.log(err))
+})
+router.delete('/:id', (req, res) => {
+  const _id = req.params.id
+  Record.findOne({ _id })
+    .then(record => record.remove())
+    .then(() => res.redirect('/'))
+    .catch(err => console.log(err))
+})
 module.exports = router
