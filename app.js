381b0ba9c54bdd2dad795c6801dbe07ebffb759c
andybox111 <andybox11111@gmail>
2023-09-06 13:05:34 +0800

    feat: home route & index.hbs

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const exphbs = require('express-handlebars')
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
+const routes = require('./routes')
 const app = express()
-
+require('./config/mongoose')
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(routes)
+
 app.listen(process.env.PORT, () => {
-  console.log(`app is running on port http://localhost:${process.env.PORT}`)
+  console.log(`app is running on http://localhost:${process.env.PORT}`)
 })
---
+++ routes/modules/home.js
@@ -0,0 +1,29 @@
+const express = require('express')
+
+const router = express.Router()
+
+const Record = require('../../models/record')
+const Category = require('../../models/category')
+
+router.get('/', (req, res) => {
+  //const userId = req.user._id
+ Record.find()
+    .lean()
+    .then(records => {
+      let totalAmount = 0
+      return Promise.all(Array.from({ length: records.length }, (_, i) => {
+        totalAmount += records[i].amount
+        return Category.findOne({ _id: records[i].categoryId }).lean()
+          .then(category => {
+            records[i].icon = category.icon
+            return records[i]
+          })
+
+      })).then((records) => {
+        res.render('index', { records, totalAmount })
+      })  
+    })
+    .catch(error => console.error(error)) 
+})
+
+module.exports = router
